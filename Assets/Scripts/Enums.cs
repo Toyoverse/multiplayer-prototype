95db1d60e198dbd8d2be89c820d9b349d6972e04
Jhone Marra <jhonemetal.marra@gmail.com>
2022-11-21 19:07:44 -0300

    Enums and Structs classes;

---
+++ Assets/Scripts/Enums.cs
@@ -0,0 +1,24 @@
+
+public enum CARD_TYPE
+{
+    NONE = 0,
+    ROCK = 1,
+    PAPER = 2,
+    SCISSOR = 3
+}
+
+public enum MATCH_RESULT
+{
+    NONE = 0,
+    LOSE = 1,
+    DRAW = 2,
+    WIN = 3
+}
+
+public enum MESSAGE_TYPE
+{
+    NONE = 0,
+    CARD_CHOICE = 1,
+    MATCH_RESULT = 2,
+    STRING = 3
+}
---
+++ Assets/Scripts/Structs.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using FishNet.Broadcast;
+using FishNet.Object;
+
+public struct NetworkMessage : IBroadcast
+{
+    //client and obj id who send || if send from server, client and obj id to receive
+    public int ClientID;
+    public int ObjectID;
+    public int MessageType;
+    public string Content;
+}
+
+public struct Match_Info
+{
+    public List<GameChoice> choices; //ObjectID, Choice
+    public bool isDraw;
+    public int winnerObjectID;
+    public int loserObjectID;
+    public int winnerClientID;
+    public int loserClientID;
+}
